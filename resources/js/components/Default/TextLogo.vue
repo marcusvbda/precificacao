f59b6d7c48b081a02eb5d6fd6f806ea7602d3404
mvbassalobre <bassalobre.vinicius@gmail.com>
2022-09-22 11:46:12 -0300

    add theme

--- resources/js/components/Default/TextLogo.vue
+++ resources/js/components/Default/TextLogo.vue
@@ -1,9 +1,6 @@
 <template>
     <h1 class="logo" :style="{ fontSize: `${size}px` }">
         {{ app_name }}
-        <small v-if="complete">
-            <b>{{ app_description }}</b>
-        </small>
     </h1>
 </template>
 <script>
@@ -12,19 +9,12 @@ export default {
         size: {
             type: String,
             default: '60',
-        },
-        complete: {
-            type: Boolean,
-            default: true,
-        },
+        }
     },
     computed: {
         app_name() {
             return laravel.config.name
-        },
-        app_description() {
-            return laravel.config.description
-        },
+        }
     },
 }
 </script>
@@ -37,10 +27,11 @@ export default {
     display: flex;
     align-items: center;
     position: relative;
-    color: #001737;
-    // text-transform: lowercase;
+    font-weight: 900;
+    color: #3f3f3f;
     font-weight: bolder;
     margin-bottom: 0;
+
     b {
         color: $primary;
         font-size: 50%;
